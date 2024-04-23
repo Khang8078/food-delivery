d6cf76d590aa684b906944c4f83420a383d28d03
Khang Huynh <khanghuynh8078@gmail.com>
2024-04-22 22:54:16 -0700

    front end

---
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,30 @@
+//import React from 'react'
+import { useContext } from 'react';
+import './FoodDisplay.css';
+import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
+import PropTypes from 'prop-types'; // Import PropTypes
+
+const FoodDisplay = ({category}) => {
+
+    const {food_list} = useContext(StoreContext)
+  return (
+    <div className='food-display' id='food-display'>
+        <h2>Top dishes near you</h2>
+        <div className="food-display-list">
+            {food_list.map((item, index) => {
+                if(category==="All" || category===item.category){
+                    return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+                }
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
+
+// Add prop types validation
+FoodDisplay.propTypes = {
+    category: PropTypes.string.isRequired, // Assuming category is a string
+  };
