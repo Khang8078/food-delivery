08f4482b5cb7afa5e176959c522a310fad861f9e
Khang Huynh <khanghuynh8078@gmail.com>
2024-04-25 00:44:27 -0700

    working on admin panel

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,24 @@
 //import React from 'react'
+import { assets } from "../../assets/assets";
 import "./Sidebar.css";
 const Sidebar = () => {
   return (
-   <div></div>
+   <div className="sidebar">
+        <div className="sidebar-options">
+            <div className="sidebar-option">
+                <img src={assets.add_icon} alt="" />
+                <p>Add Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order_icon} alt="" />
+                <p>List Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order_icon} alt="" />
+                <p>Orders</p>
+            </div>
+        </div>
+   </div>
   )
 }
 
