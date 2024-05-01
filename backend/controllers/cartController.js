81917e8ca673d089651d3e5b904aa3174e2f6b7f
Khang Huynh <khanghuynh8078@gmail.com>
2024-05-01 16:32:26 -0700

    Create shop cart functionality

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,19 @@
+import userModel from "../models/userModel.js";
+
+// add items
+const addToCart = async (req,res) => {
+
+}
+
+
+// remove items from user cart 
+const removeFromCart = async (req,res) => {
+
+}
+
+// fetch use cart data 
+const getCart = async (req,res) => {
+
+}
+
+export {addToCart, removeFromCart, getCart}
