b4e4d813f54b96df360f555e5646d368d911a1cb
Khang Huynh <khanghuynh8078@gmail.com>
2024-05-13 00:15:33 -0700

    Create user order page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,9 @@
 // import React from 'react'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 export const PlaceOrder = () => {
   const {getTotalCartAmount, token, food_list,cartItems, url} = useContext(StoreContext);
   
@@ -49,6 +50,16 @@ export const PlaceOrder = () => {
     }
   }
   
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    if(!token){
+      navigate('/cart')
+    }
+    else if(getTotalCartAmount()===0){
+      navigate('/cart')
+    }
+  },[token])
   
   return (
     <form onSubmit={placeorder} className='place-order'>
